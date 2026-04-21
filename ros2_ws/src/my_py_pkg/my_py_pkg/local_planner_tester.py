c76abe1cef6cff96f7c66836df907e439ff5adf7
Luke Kieser <luke.kieser@gmail.com>
2026-04-21 09:35:49 -0400

    changes made to local planner. Not very effective

--- ros2_ws/src/my_py_pkg/my_py_pkg/local_planner_tester.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/local_planner_tester.py
@@ -18,32 +18,40 @@ class LocalPlannerTesterNode(Node):
     # send the cmd_vel message every time the goal is updated
     def goal_callback(self, msg: PoseStamped):
         # make sure change is significant enough
-        if abs(self.last_changed_x - msg.pose.position.x) < 0.3 or abs(self.last_changed_y - msg.pose.position.y) < 0.3: # *********maybe change this to an AND
-            return
-
-        self.last_changed_x = msg.pose.position.x
-        self.last_changed_y = msg.pose.position.y
-        # recieve the x,y back as the angular z and linear x
-        temp_x, temp_y = self.compute_cmd(self.last_changed_x, self.last_changed_y)
-        # publish the cmd_vel message
-        vel_msg = Twist()
-        vel_msg.linear.x = temp_x
-        vel_msg.angular.z = temp_y
-        self.cmd_pub_.publish(vel_msg)
+        if abs(self.last_changed_x - msg.pose.position.x) < 0.3 and abs(self.last_changed_y - msg.pose.position.y) < 0.3:
+            # publish the cmd_vel message
+            vel_msg = Twist()
+            vel_msg.linear.x = float(self.last_changed_x)
+            vel_msg.angular.z = float(self.last_changed_y)
+            self.cmd_pub_.publish(vel_msg)
+        else:
+            self.last_changed_x = msg.pose.position.x
+            self.last_changed_y = msg.pose.position.y
+            # recieve the x,y back as the angular z and linear x
+            temp_x, temp_y = self.compute_cmd(self.last_changed_x, self.last_changed_y)
+            # publish the cmd_vel message
+            vel_msg = Twist()
+            vel_msg.linear.x = float(temp_x)
+            vel_msg.angular.z = float(temp_y)
+            #self.cmd_pub_.publish(vel_msg)
 
     def compute_cmd(self, x, y):
         distance = math.sqrt(x*x + y*y)
         angle_error = math.atan2(y, x)
 
+        self.get_logger().info(f"distance from bot: {distance}")
+
         # set follow distance
-        follow_distance = 1.0
+        follow_distance = 2.0
         distance_error = distance - follow_distance
+
+        self.get_logger().info(f"distance error: {distance_error}")
 
         # factors for proportional control
         k_lin = 0.6
         k_ang = 1.5
-        max_lin = 0.5
-        max_ang = 0.8
+        max_lin = 0.26 # 0.56
+        max_ang = 0.4 # 0.8
 
         angular_z = k_ang * angle_error
         linear_x = k_lin * distance_error
@@ -59,6 +67,11 @@ class LocalPlannerTesterNode(Node):
         # clamp outputs
         angular_z = max(min(angular_z, max_ang), -max_ang)
         linear_x = max(min(linear_x, max_lin), 0.0)
+
+        # if distance_error <= 0:
+        #     self.get_logger().info(f"Close enough-------------------")
+        #     angular_z = 0
+        #     linear_x = 0
 
         return linear_x, angular_z
 
